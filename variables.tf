6a764458b4d26b305f64590f2704369c57f02a13
Slava Goltser <slavikg@gmail.com>
2025-06-19 23:35:38 -0400

    declare some variables

--- variables.tf
+++ variables.tf
@@ -1,4 +1,23 @@
+variable "domain" {
+  type    = string
+  description = "Domain where proxy lives"
+  default = "aws.theslava.com"
+}
+
+variable "profile" {
+  type = string
+  description = "AWS profile for auth"
+  default = "terraform"
+}
+
+variable "region" {
+  type = string
+  description = "Region where everything should be deployed"
+  default = "us-west-2"
+}
+
 variable "services" {
   type    = list(string)
+  description = "Services to proxy"
   default = ["homeassistant", "jellyfin"]
 }
